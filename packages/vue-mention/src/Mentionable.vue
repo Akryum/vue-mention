56026eae3c347490ce07b1632bc288caba6b199c
taiyop <akashitaiyo@gmail.com>
2020-07-14 23:20:43 +0900

    fix: disable popover when input is email & enale popover when previous input is space or new line

--- packages/vue-mention/src/Mentionable.vue
+++ packages/vue-mention/src/Mentionable.vue
@@ -242,6 +242,9 @@ export default {
       if (index >= 0) {
         const { key, keyIndex } = this.getLastKeyBeforeCaret(index)
         const searchText = this.getLastSearchText(index, keyIndex)
+        if (!(keyIndex < 1 || /\r\n|\r|\n|\s/.test(this.getValue()[keyIndex - 1]))) {
+          return false
+        }
         if (searchText != null) {
           this.openMenu(key, keyIndex)
           this.searchText = searchText

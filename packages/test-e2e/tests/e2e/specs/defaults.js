9b574a4965b9ab441b18707641f051d530969a7e
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-12-28 15:21:50 +0100

    test(e2e): uncomment

--- packages/test-e2e/tests/e2e/specs/defaults.js
+++ packages/test-e2e/tests/e2e/specs/defaults.js
@@ -98,19 +98,19 @@ describe('with default props', () => {
     cy.get('.preview').should('contain', '@akryumabc')
   })
 
-  // it('show suggestion when previous input is space', () => {
-  //   cy.visit('/defaults')
-  //   cy.get('.input').type(' @zzz')
-  //   cy.get('.popover').should('be.visible')
-  //   cy.get('.input').type('　@zzz')
-  //   cy.get('.popover').should('be.visible')
-  //   cy.get('.input').type('\n@zzz')
-  //   cy.get('.popover').should('be.visible')
-  // })
-  //
-  // it('does not show suggestion when text is email address', () => {
-  //   cy.visit('/defaults')
-  //   cy.get('.input').type('aaa@zzz.com')
-  //   cy.get('.popover').should('not.exist')
-  // })
+  it('show suggestion when previous input is space', () => {
+    cy.visit('/defaults')
+    cy.get('.input').type(' @zzz')
+    cy.get('.popover').should('be.visible')
+    cy.get('.input').type('　@zzz')
+    cy.get('.popover').should('be.visible')
+    cy.get('.input').type('\n@zzz')
+    cy.get('.popover').should('be.visible')
+  })
+
+  it('does not show suggestion when text is email address', () => {
+    cy.visit('/defaults')
+    cy.get('.input').type('aaa@zzz.com')
+    cy.get('.popover').should('not.exist')
+  })
 })

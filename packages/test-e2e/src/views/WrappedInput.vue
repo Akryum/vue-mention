b172e88979ae04c685bcf855a7d961aa02b9b581
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-12-28 15:10:54 +0100

    test(e2e): wrapped input

---
+++ packages/test-e2e/src/views/WrappedInput.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  components: {
+    WrappedTextarea: {
+      render (h) {
+        return <div>
+          <textarea class="input"/>
+        </div>
+      },
+    },
+
+    WrappedInput: {
+      render (h) {
+        return <div>
+          <input class="input"/>
+        </div>
+      },
+    },
+  },
+
+  data () {
+    return {
+      items: [
+        {
+          value: 'akryum',
+          firstName: 'Guillaume',
+        },
+        {
+          value: 'posva',
+          firstName: 'Eduardo',
+        },
+        {
+          value: 'atinux',
+          firstName: 'Sébastien',
+        },
+      ],
+      useTextarea: true,
+    }
+  },
+}
+</script>
+
+<template>
+  <div
+    class="demo"
+  >
+    <label class="toggle">
+      <input type="checkbox" v-model="useTextarea">
+      Use textarea
+    </label>
+
+    <Mentionable
+      :keys="['@']"
+      :items="items"
+    >
+      <WrappedTextarea v-if="useTextarea" />
+
+      <WrappedInput v-else />
+    </Mentionable>
+  </div>
+</template>

0948b5ff4edcd5c6dc1a1cce6341475de8822a5b
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-03 21:07:59 +0100

    feat: switch build to vite

---
+++ packages/vue-mention/vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.js'),
+      name: 'VueMention',
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        'v-tooltip',
+      ],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'v-tooltip': 'VTooltip',
+        },
+      },
+    },
+  },
+  define: {
+    VERSION: JSON.stringify(require('./package.json').version),
+  },
+})

e51498e1fff864e1ad90de9d118f20e92ef606ea
Ole Marius Løset <olemariusloset@gmail.com>
2022-04-25 20:49:54 +0200

    test: add remove applied mention test

--- packages/test-e2e/tests/e2e/specs/allow-space.js
+++ packages/test-e2e/tests/e2e/specs/allow-space.js
@@ -7,4 +7,16 @@ describe('with allow-space prop', () => {
     cy.get('.input').type('{enter}')
     cy.get('.preview').should('contain', '@space invader')
   })
+
+  it.only('do not show suggestions when deleting applied mention', () => {
+    cy.visit('/allow-space')
+    cy.get('.input').type('abc @space in')
+    cy.get('.v-popper__popper').should('be.visible')
+      .should('contain', 'space invader')
+    cy.get('.input').type('{enter}')
+    cy.get('.input')
+      .type('{backspace}'.repeat(14))
+    cy.get('.v-popper__popper').should('not.be.visible')
+    cy.get('.preview').should('not.contain', '@space invader')
+  })
 })
